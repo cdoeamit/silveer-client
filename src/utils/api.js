25577045a81a0ca301b42e3aca6d6d688f4e6a0d
Amit <your-email@example.com>
2025-12-07 00:08:28 +0530

    update frontend

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://silver-server-1.onrender.com/api/health',
+  baseURL: 'https://silver-server-1.onrender.com/',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
