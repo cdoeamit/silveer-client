50d2462e3410d52cdf8f65ed4f243728971408fd
Amit <your-email@example.com>
2025-12-07 13:24:29 +0530

    feat: implement vercel proxy for api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://silver-server-1.onrender.com/api/',
+  baseURL: '/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
