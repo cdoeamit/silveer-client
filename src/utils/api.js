e429736b5bc5f32220b569c14198a0c34e6efe3d
Amit <your-email@example.com>
2025-12-07 11:57:47 +0530

    fix: update api url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://silver-server-1.onrender.com/',
+  baseURL: 'https://silver-server-1.onrender.com/api/',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
